b0708dd9f631c46a98f98e614e54976931dbeb36
szegefei <szegefei@gmail.com>
2021-01-05 20:35:26 +0000

    update foraging and voting

--- internal/clients/team6/foraging.go
+++ internal/clients/team6/foraging.go
@@ -2,7 +2,6 @@ package team6
 
 import (
 	"math/rand"
-	"sort"
 
 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
 )
@@ -119,28 +118,4 @@ func (c *client) ForageUpdate(forageDecision shared.ForageDecision, outcome shar
 		forageDecision.Contribution,
 		outcome,
 	)
-}
-
-// Pair is used for sorting the friendship
-type Pair struct {
-	Key   shared.ClientID
-	Value uint
-}
-
-// PairList is a slice of Pairs that implements sort.Interface to sort by Value.
-type PairList []Pair
-
-func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
-func (p PairList) Len() int           { return len(p) }
-func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
-
-// A function to turn a map into a PairList, then sort and return it.
-func sortMapByValue(m map[shared.ClientID]uint) PairList {
-	p := make(PairList, len(m))
-	i := 0
-	for k, v := range m {
-		p[i] = Pair{k, v}
-	}
-	sort.Sort(p)
-	return p
 }
--- internal/clients/team6/voting.go
+++ internal/clients/team6/voting.go
@@ -1,6 +1,8 @@
 package team6
 
 import (
+	"sort"
+
 	"github.com/SOMAS2020/SOMAS2020/internal/common/rules"
 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
 )
@@ -34,15 +36,26 @@ func (c *client) GetVoteForElection(roleToElect shared.Role) []shared.ClientID {
 	return returnList
 }
 
-// ########################
-// ######  Voting  ########
-// ########################
-// GetVoteForRule returns the client's vote in favour of or against a rule.
-// func (c *client) GetVoteForRule(ruleName string) bool {
-// 	for _, val := range c.favourRules {
-// 		if val == ruleName {
-// 			return true
-// 		}
-// 	}
-// 	return false
-// }
+// Pair is used for sorting the friendship
+type Pair struct {
+	Key   shared.ClientID
+	Value uint
+}
+
+// PairList is a slice of Pairs that implements sort.Interface to sort by Value.
+type PairList []Pair
+
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+
+// A function to turn a map into a PairList, then sort and return it.
+func sortMapByValue(m map[shared.ClientID]uint) PairList {
+	p := make(PairList, len(m))
+	i := 0
+	for k, v := range m {
+		p[i] = Pair{k, v}
+	}
+	sort.Sort(p)
+	return p
+}
