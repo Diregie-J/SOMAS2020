288ed5acfb1416764846dca22b10545218a3c729
szegefei <szegefei@gmail.com>
2021-01-05 20:32:32 +0000

    Add files via upload
    update foraging

--- internal/clients/team6/foraging.go
+++ internal/clients/team6/foraging.go
@@ -7,19 +7,57 @@ import (
 	"github.com/SOMAS2020/SOMAS2020/internal/common/shared"
 )
 
-func chooseForageType() int {
-	return int(shared.DeerForageType)
+// ForageHistory stores our forage history
+// type ForageHistory map[shared.ForageType][]ForageResults
+
+// type ForageResults struct {
+// 	forageIn     shared.Resources
+// 	forageReturn shared.Resources
+// }
+func (result ForageResults) calcROI() float64 {
+	if result.forageIn == 0 {
+		return 0
+	} else {
+		return float64(result.forageReturn/result.forageIn) - 1
+	}
 }
 
-func (c client) changeForageType() shared.ForageType { //TODO
+func (c client) changeMultiplier() float64 {
+	for _, results := range forageHistory {
+		var lastRoi float64 = 0
+		var lastlastRoi float64 = 0
+		for _, result := range results {
+			if result.turn == c.ServerReadHandle.GetGameState().Turn-2 {
+				lastlastRoi = result.calcROI()
+			}
+			if result.turn == c.ServerReadHandle.GetGameState().Turn-1 {
+				lastRoi = result.calcROI()
+			}
+			if lastRoi != 0 && lastlastRoi != 0 {
+				//it means last round and the round before last round are using the same forage type
+				if lastlastRoi < lastRoi {
+					c.config.multiplier += 0.1
+				} else {
+					c.config.multiplier += 0.1
+				}
+			}
+		}
+	}
+	return c.config.multiplier
+}
+func (c *client) changeForageType() shared.ForageType {
+	//fishing is a safer choice if we contributed a lot
+	if c.config.multiplier > 0.5 {
+		return shared.FishForageType
+	}
 	return shared.DeerForageType
 }
 
-func (c client) decideContribution() shared.Resources {
-	var multiplier shared.Resources = 0.8
+func (c *client) decideContribution() shared.Resources {
+
 	var safetyBuffer shared.Resources = 10
 	ourResources := c.ServerReadHandle.GetGameState().ClientInfo.Resources
-	return multiplier * (ourResources - safetyBuffer)
+	return shared.Resources(c.changeMultiplier()) * (ourResources - safetyBuffer)
 }
 
 func (c *client) randomForage() shared.ForageDecision {
@@ -63,15 +101,24 @@ func (c *client) DecideForage() (shared.ForageDecision, error) {
 }
 
 func (c *client) ForageUpdate(forageDecision shared.ForageDecision, outcome shared.Resources) {
+	currTurn := c.ServerReadHandle.GetGameState().Turn
+
 	c.forageHistory[forageDecision.Type] =
 		append(
 			c.forageHistory[forageDecision.Type],
 			ForageResults{
 				forageIn:     forageDecision.Contribution,
 				forageReturn: outcome,
-				forageROI:    float64(forageDecision.Contribution/outcome - 1),
+				turn:         currTurn,
 			},
 		)
+
+	c.Logf(
+		"Forage History Updated: Type %v ,Conribution: %v ,Return: %v",
+		forageDecision.Type,
+		forageDecision.Contribution,
+		outcome,
+	)
 }
 
 // Pair is used for sorting the friendship
