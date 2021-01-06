a133fe950d15b68523b5f592e648f83913ecc815
szegefei <szegefei@gmail.com>
2021-01-06 23:53:52 +0000

    Update iigo.go

--- internal/clients/team6/iigo.go
+++ internal/clients/team6/iigo.go
@@ -17,14 +17,14 @@ func (c *client) GetClientSpeakerPointer() roles.Speaker {
 	return &speaker{client: c}
 }
 
-func (c *client) ReceiveCommunication(sender shared.ClientID, data map[shared.CommunicationFieldName]shared.CommunicationContent) {
-	for fieldName, content := range data {
-		switch fieldName {
-		case shared.TaxAmount:
-			c.config.payingTax = shared.Resources(content.IntegerData)
-		} //add sth else
-	}
-}
+// func (c *client) ReceiveCommunication(sender shared.ClientID, data map[shared.CommunicationFieldName]shared.CommunicationContent) {
+// 	for fieldName, content := range data {
+// 		switch fieldName {
+// 		case shared.TaxAmount:
+// 			c.config.payingTax = shared.Resources(content.IntegerData)
+// 		} //add sth else
+// 	}
+// }
 
 func (c *client) MonitorIIGORole(roleName shared.Role) bool {
 	return false
